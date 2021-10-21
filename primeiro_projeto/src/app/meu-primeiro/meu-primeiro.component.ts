fbeedfb2fae1dfcb2e60467ee7ddd18c3eea6287
Cassio Szerniak <cassioszerniak@gmail.com>
2021-10-21 00:48:47 -0300

    Update meu-primeiro.component.ts

--- primeiro_projeto/src/app/meu-primeiro/meu-primeiro.component.ts
+++ primeiro_projeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -1,9 +1,12 @@
 import { Component } from "@angular/core";
 
+
+//decorator ou anotação(C# ou Java), passa informação para o compilador do typescript (neste caso), dizendo para o Angular que essa classe é um componente do Angular
 @Component({
     selector: 'meu-primeiro-component',
     template: `
         <p>Meu primeiro component com Angular 2!</p>
     `
 })
+
 export class MeuPrimeiroComponent {}
